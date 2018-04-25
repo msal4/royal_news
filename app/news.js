4e46f69c61324985bbff3c6a098fdc1b8c4e2662
Mohammed Salman <msal4@outlook.com>
2018-04-25 08:47:18 -0700

    Update news.js

--- app/news.js
+++ app/news.js
@@ -2,12 +2,8 @@ const url =
   'https://newsapi.org/v2/top-headlines?country=us&apiKey=79af8a0825ba4443adf9c1f76f8913cb';
 
 export async function getNews() {
-  try {
-    let articles = await fetch(url).then(response => response);
-
-    let result = await articles.json();
-    articles = null;
-    return result.articles;
+  try { 
+    return await fetch(url).then(respones => response.json()).articles;
   } catch (error) {
     throw error;
   }
